167952f685d4748a4cc682b4d6ba9c6d17da7e82
Nate Gillis <ngillis13@gmail.com>
2021-03-17 16:50:05 -0600

    another try

---
+++ grecery-store/src/components/CartList.vue
@@ -0,0 +1,99 @@
+<template>
+<div class="wrapper">
+  <div class="products">
+    <div class="product" v-for="product in products" :key="product.id">
+      <div class="info">
+        <h1>{{product.name}}</h1>
+        <p>{{product.country}}</p>
+      </div>
+      <div class="image">
+        <img :src="'/images/products/'+product.image">
+      </div>
+      <div class="price">
+        <h2>{{product.price}}</h2>
+        <button @click="removeFromCart(product)">Remove from Cart</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CartList',
+  props: {
+    products: Array
+  },
+  methods: {
+    removeFromCart(product){
+
+      console.log(this.$root._data.cart.indexOf(product))
+      this.$root._data.cart.splice(this.$root._data.cart.indexOf(product), 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.products {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.product {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.product img {
+  border: 2px solid #333;
+  height: 250px;
+  width: 200px;
+  object-fit: cover;
+}
+
+.product .image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+
+.info {
+  background: #F2921D;
+  color: #000;
+  padding: 10px 30px;
+  height: 80px;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+
+.price {
+  display: flex;
+}
+
+
+.auto {
+  margin-left: auto;
+}
+</style>
---
+++ grecery-store/src/views/Cart.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+  <CartList :products="cart" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+
+  computed: {
+    cart(){
+      return this.$root.$data.cart
+    }
+  },
+}
+
+
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
